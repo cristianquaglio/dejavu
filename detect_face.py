1bfd7b84d1eb42b64b0e4bb17ccc45b9061df7f8
Cristian Quagliozzi <cristianquaglio@gmail.com>
2025-02-25 02:47:59 -0300

    feat: First commit

--- detect_face.py
+++ detect_face.py
@@ -1,43 +1,33 @@
 import cv2
 import dlib
 
-# Cargar el detector de rostros de dlib
 detector = dlib.get_frontal_face_detector()
 
-# Cargar la imagen
 img = cv2.imread("data/images/04.jpg")
 
-# Convertir la imagen a escala de grises
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Detectar los rostros en la imagen
 faces = detector(gray)
 
-# Dibujar rectángulos alrededor de los rostros detectados
 for face in faces:
     x, y, w, h = (face.left(), face.top(), face.width(), face.height())
-    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)  # Rectángulo verde
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)  # green rectangle
 
-# Redimensionar la imagen para que se ajuste a la pantalla
 height, width = img.shape[:2]
-new_width = 800  # Nuevo ancho deseado
+new_width = 800  # desired width
 new_height = int((new_width / width) * height)
 resized_img = cv2.resize(img, (new_width, new_height))
 
-# Crear una ventana que se pueda redimensionar
-cv2.namedWindow("Rostros detectados", cv2.WINDOW_NORMAL)
+cv2.namedWindow("Detected faces", cv2.WINDOW_NORMAL)
 
-# Mostrar la imagen con los rostros detectados
-cv2.imshow("Rostros detectados", resized_img)
+cv2.imshow("Detected faces", resized_img)
 
-# Esperar hasta que se presione una tecla para cerrar la ventana
 while True:
-    key = cv2.waitKey(1)  # Espera por un poco, 1ms
+    key = cv2.waitKey(1)  # wait 1ms
     if (
         key == ord("q")
-        or cv2.getWindowProperty("Rostros detectados", cv2.WND_PROP_VISIBLE) < 1
+        or cv2.getWindowProperty("Detected faces", cv2.WND_PROP_VISIBLE) < 1
     ):
-        break  # Romper el bucle si la ventana se cierra o si se presiona 'q'
+        break
 
-# Cerrar todas las ventanas y terminar el proceso
 cv2.destroyAllWindows()
